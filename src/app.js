0b18729667ea9cd475e34181f66f2a66a2799f05
Bill Byte <bill.byte@pm.me>
2023-02-06 23:23:03 +0100

    learning js

--- src/app.js
+++ src/app.js
@@ -8,3 +8,24 @@ button.addEventListener('click',(event) => {
     // document.body.classList.toggle('color')
     // when in .css file we have .color{background: black;}
 })
+
+// function findX(){
+//     var x = 1
+//     {
+//         let x = 2
+//         console.log('inside: ' + x)
+//     }
+//     console.log('outside: ' + x)
+// }
+
+// findX()
+
+const time = 12;
+
+if (time < 12) {
+    console.log('good morning')
+} else if (time === 12) {
+    console.log('noon')
+} else {
+    console.log('good afternoon')
+}

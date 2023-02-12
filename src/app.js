216cb3e9322b3c26263deaee9924f6abe0df2dfd
Bill Byte <bill.byte@pm.me>
2023-02-12 10:17:42 +0100

    changes

--- src/app.js
+++ src/app.js
@@ -3,29 +3,14 @@ let color = 'white';
 const button = document.getElementById('dark_mode');
 
 button.addEventListener('click',(event) => {
-    color = color === 'white' ? 'black' : "white";
+    // using ternary operators: color = color === 'white' ? 'black' : "white";
+    if (color === 'white') {
+        color = 'black';
+    } else {
+        color = 'white';
+    }
     document.body.style.background = color;
     // document.body.classList.toggle('color')
     // when in .css file we have .color{background: black;}
 })
 
-// WHY TO NOT USE 'var'
-// function findX(){
-//     var x = 1
-//     {
-//         let x = 2
-//         console.log('inside: ' + x)
-//     }
-//     console.log('outside: ' + x)
-// }
-// findX()
-
-const time = 12;
-
-if (time < 12) {
-    console.log('good morning')
-} else if (time === 12) {
-    console.log('noon')
-} else {
-    console.log('good afternoon')
-}

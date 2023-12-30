553124416f5a68707974213c93232ecb47b3adad
Bill Byte <bill.byte@pm.me>
2023-12-30 09:22:04 +0100

    refresh

--- src/app.js
+++ src/app.js
@@ -1,35 +1,33 @@
-let color = 'white';
+let color = "white"
 
-const button = document.getElementById('dark_mode');
+const button = document.getElementById("dark_mode")
 
-button.addEventListener('click', function() {
-    // using ternary operators: color = color === 'white' ? 'black' : "white";
-    if (color === 'white') {
-        color = 'black';
-    } else {
-        color = 'white';
-    }
-    document.body.style.background = color;
-    // document.body.classList.toggle('color')
-    // when in .css file we have .color{background: black;}
+button.addEventListener("click", function () {
+	// using ternary operators: color = color === 'white' ? 'black' : 'white';
+	if (color === "white") {
+		color = "black"
+	} else {
+		color = "white"
+	}
+	document.body.style.background = color
+	// document.body.classList.toggle('color')
+	// when in .css file we have .color{background: black;}
 })
 
-const msgStatus = document.querySelector('.msg-status')
+const msgStatus = document.querySelector(".msg-status")
 
-if(document.location.search === '?mail_status=sent'){
-    msgStatus.classList.add('success')
-    msgStatus.textContent = 'message sent'
+if (document.location.search === "?mail_status=sent") {
+	msgStatus.classList.add("success")
+	msgStatus.textContent = "message sent"
 
-    setTimeout(() => {
-        msgStatus.classList.remove('success')
-    }, 3000)
-}
+	setTimeout(() => {
+		msgStatus.classList.remove("success")
+	}, 3000)
+} else if (document.location.search === "?mail_status=error") {
+	msgStatus.classList.add("error")
+	msgStatus.textContent = "message not sent"
 
-if(document.location.search === '?mail_status=error'){
-    msgStatus.classList.add('error')
-    msgStatus.textContent = 'message not sent'
-
-    setTimeout(() => {
-        msgStatus.classList.remove('error')
-    }, 3000)
+	setTimeout(() => {
+		msgStatus.classList.remove("error")
+	}, 3000)
 }

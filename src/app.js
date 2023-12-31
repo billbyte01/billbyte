3fe02894609ce878d77d12ab1bc0823140b23372
Bill Byte <bill.byte@pm.me>
2023-12-31 22:11:51 +0100

    up

--- src/app.js
+++ src/app.js
@@ -1,33 +1,33 @@
-let color = "white"
+let color = 'white'
 
-const button = document.getElementById("dark_mode")
+const button = document.getElementById('dark_mode')
 
-button.addEventListener("click", function () {
+button.addEventListener('click', function () {
 	// using ternary operators: color = color === 'white' ? 'black' : 'white';
-	if (color === "white") {
-		color = "black"
+	if (color === 'white') {
+		color = 'black'
 	} else {
-		color = "white"
+		color = 'white'
 	}
 	document.body.style.background = color
 	// document.body.classList.toggle('color')
 	// when in .css file we have .color{background: black;}
 })
 
-const msgStatus = document.querySelector(".msg-status")
+const msgStatus = document.querySelector('.msg-status')
 
-if (document.location.search === "?mail_status=sent") {
-	msgStatus.classList.add("success")
-	msgStatus.textContent = "message sent"
+if (document.location.search === '?mail_status=sent') {
+	msgStatus.classList.add('success')
+	msgStatus.textContent = 'message sent'
 
 	setTimeout(() => {
-		msgStatus.classList.remove("success")
+		msgStatus.classList.remove('success')
 	}, 3000)
-} else if (document.location.search === "?mail_status=error") {
-	msgStatus.classList.add("error")
-	msgStatus.textContent = "message not sent"
+} else if (document.location.search === '?mail_status=error') {
+	msgStatus.classList.add('error')
+	msgStatus.textContent = 'message not sent'
 
 	setTimeout(() => {
-		msgStatus.classList.remove("error")
+		msgStatus.classList.remove('error')
 	}, 3000)
 }
